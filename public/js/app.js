416722f27f13935d29326972fc75c02297f497c5
Iván Espósito <ivan@scala.ly>
2021-04-05 12:54:55 -0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading..'
     messageTwo.textContent = ''
 
-    fetch(`http://localhost:3000/weather?adress=${encodeURIComponent(adress)}`).then((response) => {
+    fetch(`/weather?adress=${encodeURIComponent(adress)}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error 
